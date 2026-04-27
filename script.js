88751f64a440ccad85f6f0c0a4f382a8a9200b27
Chakshu Bhatia <chakshu.bhatia1000@gmail.com>
2026-04-28 04:06:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".signup-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const button = form.querySelector("button");
+  const originalText = button.textContent;
+
+  button.textContent = "Request Received";
+  button.disabled = true;
+
+  window.setTimeout(() => {
+    button.textContent = originalText;
+    button.disabled = false;
+    form.reset();
+  }, 2200);
+});
